8e1488a8f4c487b96abf06d7f51d97ddaf070cf7
Ahmet Türkmen <f.ahmet.turkmen@icloud.com>
2020-08-17 16:02:54 +0300

    add example client codes
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -52,9 +52,74 @@ func main() {
 	defer conn.Close()
 
 	client := wg.NewWireguardClient(conn)
-
-	_, err = client.GenPrivateKey(context.Background(), &wg.PrivKeyReq{PrivateKeyName: "test_private_key"})
+	//privatekey generation example
+	privKeyResp, err := client.GenPrivateKey(context.Background(), &wg.PrivKeyReq{PrivateKeyName: "random_privatekey"})
 	if err != nil {
 		fmt.Println(fmt.Sprintf("Error happened in creating private key %v", err))
+		panic(err)
 	}
+	fmt.Println(privKeyResp.Message)
+
+	// publickey generation example
+	publicKeyResp, err := client.GenPublicKey(context.Background(), &wg.PubKeyReq{PrivKeyName: "random_privatekey", PubKeyName: "random_publickey"})
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error happened in creating public key %s", err.Error()))
+		//panic(err)
+	}
+	if publicKeyResp != nil {
+		fmt.Println(publicKeyResp.Message)
+	}
+
+	privateKey, err := client.GetPrivateKey(context.Background(), &wg.PrivKeyReq{PrivateKeyName: "random_privatekey"})
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Get content of private key error %s", err.Error()))
+		//panic(err)
+	}
+	if privateKey != nil {
+		fmt.Println(privateKey.Message)
+	}
+
+	publicKey, err := client.GetPublicKey(context.Background(), &wg.PubKeyReq{PubKeyName: "random_publickey"})
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Get content of public key error %s", err.Error()))
+		//panic(err)
+	}
+	if publicKey != nil {
+		fmt.Println(publicKey.Message)
+	}
+
+	// insert content of privatekey in to initInterface
+	interfaceGenResp, err := client.InitializeI(context.Background(), &wg.IReq{
+		Address:    "10.0.2.1/24",
+		ListenPort: 4000,
+		SaveConfig: true,
+		PrivateKey: privateKey.Message,
+		Eth:        "eth0",
+		IName:      "wg1",
+	})
+	if err != nil {
+		fmt.Println(fmt.Sprintf(" Initializing interface error %v", err.Error()))
+
+	}
+	if interfaceGenResp != nil {
+		fmt.Println(interfaceGenResp.Message)
+	}
+
+	nicInfoResp, err := client.GetNICInfo(context.Background(), &wg.NICInfoReq{Interface: "wg1"})
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Getting information of interface error %s", err.Error()))
+		panic(err)
+	}
+	if nicInfoResp != nil {
+		fmt.Println(nicInfoResp.Message)
+	}
+
+	downI, err := client.ManageNIC(context.Background(), &wg.ManageNICReq{Cmd: "down", Nic: "wg1"})
+	if err != nil {
+		fmt.Println(fmt.Sprintf("down interface is failed %s", err.Error()))
+		panic(err)
+	}
+	fmt.Println(downI.Message)
+
+	//resp, err := client.GenPublicKey(context.Background(), &wg.PrivKeyReq{})
 }
